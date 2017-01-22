eeefa3f666b2d3845d5a13638124c5627df63c2e
Stewart Matheson <stew@rtmatheson.com>
2017-01-22 17:44:01 +1100

    Added a prototype schema to validate the idea of building react forms up from ViewModels

--- src/CollAction/frontend/app/index.ts
+++ src/CollAction/frontend/app/index.ts
@@ -1,2 +1,10 @@
 import "./header/style.scss"
 import "./footer/style.scss"
+
+import ProjectFormBoom from './project/form';
+
+window['CollAction'] = {
+  forms: {
+    Project : { boom: ProjectFormBoom },
+  }
+}
